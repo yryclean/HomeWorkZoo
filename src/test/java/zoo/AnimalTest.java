ce9907817bb40022a016db221c5d87b83ae6c11a
Yury Chistyakov <31385803+yryclean@users.noreply.github.com>
2021-11-12 16:55:01 +0300

    Update AnimalTest.java
    deleted comments

--- src/test/java/zoo/AnimalTest.java
+++ src/test/java/zoo/AnimalTest.java
@@ -11,17 +11,7 @@ public class AnimalTest extends TestBase {
 
     @Test(groups = {GroupTest.SMOKE, GroupTest.REGRESSION, GroupTest.SANITY}, dataProvider = "descriptions")
     public void testSpeaks(String breath, String moves, String legs) {
-        System.out.printf("Animals breath %s. Animals move and live %s. Animals have %s", breath, moves, legs);
-//        String breath = "air";
-//        String moves = "on land";
-
-//        System.out.println("Animals have: " + legs + " legs");
-//        assertEquals(4, 4);
-//        System.out.println("Animal breath: " + breath);
-//        assertEquals("air", "air");
-//        System.out.println("Animal moves and lives: " + moves);
-//        assertEquals("on land", "on land");
-
+        System.out.printf("Animals breath %s. Animals move and live %s. Animals have %s", breath, moves, legs)
     }
     @DataProvider(name = "descriptions")
     public Object[][] myDataProvider() {
